b8eb591de3e129c3ece3596f6bb4f275958682ec
black-RAM <pplanes311@gmail.com>
2024-09-04 18:45:37 +0300

    validation for user creation and updating
    page for user updating

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,45 @@
+const { body, validationResult } = require("express-validator");
 const usersStorage = require("../storages/usersStorage");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateUserMiddleware = [
+  body("firstName").trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+  body("lastName").trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+];
+
+const newUserPostController = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render("createUser", {
+      title: "Create user",
+      errors: errors.array(),
+    });
+  }
+  const { firstName, lastName } = req.body;
+  usersStorage.addUser({ firstName, lastName });
+  res.redirect("/");
+}
+
+const updateUserPostController = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render("updateUser", {
+      title: "Update user",
+      user: user,
+      errors: errors.array(),
+    });
+  }
+  const { firstName, lastName } = req.body;
+  usersStorage.updateUser(req.params.id, { firstName, lastName });
+  res.redirect("/");
+}
 
 exports.usersListGet = (req, res) => {
   res.render("index", {
@@ -13,8 +54,14 @@ exports.usersCreateGet = (req, res) => {
   });
 };
 
-exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUser({ firstName, lastName });
-  res.redirect("/");
+exports.usersCreatePost = [validateUserMiddleware, newUserPostController]
+
+exports.usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  });
 };
+
+exports.usersUpdatePost = [validateUserMiddleware, updateUserPostController]
