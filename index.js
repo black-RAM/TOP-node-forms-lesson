bd49eff88d02f58c05e952b67f679628f2b25b4a
black-RAM <pplanes311@gmail.com>
2024-09-06 20:25:39 +0300

    set extended flag to false for express.urlencoded middleware

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const usersRouter = require("./routes/usersRouter");
 
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use("/", usersRouter);
 
 const PORT = process.env.PORT || 3000;
